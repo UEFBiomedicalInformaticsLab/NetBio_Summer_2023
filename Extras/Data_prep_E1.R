0778879afdbd7559f7c5c523d8d5e6d9c4c946a3
ag1805x <ag1805x@outlook.in>
2023-08-18 15:55:53 +0300

    updates

---
+++ Extras/Data_prep_E1.R
@@ -0,0 +1,114 @@
+# Data preparation (Exercise 1, part B)
+
+
+library(tidyverse)
+library(igraph)
+
+
+# Extract the human drug targets from DrugBank
+DrugBank_Targets_idMap <- read.csv("Data_prep/targets_polypeptides_ext_id.csv", header = TRUE)
+DrugBank_Targets_idMap <- reshape(as.data.frame(DrugBank_Targets_idMap), idvar = "parent_key", timevar = "resource", direction = "wide")
+colnames(DrugBank_Targets_idMap) <- gsub("identifier.", "", colnames(DrugBank_Targets_idMap))
+DrugBank_Targets_idMap <- DrugBank_Targets_idMap[grep("_HUMAN$", x = DrugBank_Targets_idMap$`UniProt Accession`),]
+
+
+# Process STRING DB
+
+# Read protein Id mappings
+String_proteins <- read.table("Data_prep/9606.protein.aliases.v11.5.txt.gz", fill = TRUE, sep = "\t")
+colnames(String_proteins) <- c("string_protein_id", "alias", "source")
+String_proteins <- String_proteins[String_proteins$source == "Ensembl_EntrezGene",]
+
+# Read all PPI in String DB along with scores
+String_ppi <- read.table("Data_prep/9606.protein.links.detailed.v11.5.txt.gz", header = TRUE)
+
+
+# Filter all interactions with scores greater than 0 and sourced from database/experimets
+String_ppi <- String_ppi[(String_ppi$combined_score > quantile(String_ppi$combined_score, 0.95)), ] #
+
+String_ppi$from_geneSymbol <- String_proteins$alias[match(String_ppi$protein1, String_proteins$string_protein_id)]
+String_ppi$to_geneSymbol <- String_proteins$alias[match(String_ppi$protein2, String_proteins$string_protein_id)]
+
+String_ppi_Net <- na.exclude(String_ppi[, c("from_geneSymbol", "to_geneSymbol", "combined_score")])
+
+
+drug_target_Net <- String_ppi_Net[(String_ppi_Net$from_geneSymbol %in% DrugBank_Targets_idMap$GenAtlas &
+                                         String_ppi_Net$to_geneSymbol %in% DrugBank_Targets_idMap$GenAtlas),]
+
+write.csv(drug_target_Net, "Data/Human_drug_target_net.csv", row.names = FALSE)
+
+
+
+
+# Create drugs
+
+# Extract all drug target interactions in human
+DrugBank_Drug_Target <- read.csv("Data_prep/targets.csv", header = TRUE)
+DrugBank_Drug_Target <- DrugBank_Drug_Target[DrugBank_Drug_Target$organism == "Humans", ]
+colnames(DrugBank_Drug_Target)[colnames(DrugBank_Drug_Target) == "parent_key"] <- "drug_id"
+DrugBank_Drug_Target$drug_target <- DrugBank_Targets_idMap$GenAtlas[match(DrugBank_Drug_Target$id, DrugBank_Targets_idMap$parent_key)]
+
+
+# Keep only approved drugs
+DrugBank_drug_groups <- read.csv("Data_prep/drug_groups.csv", header = TRUE, check.names = FALSE)
+DrugBank_drug_groups <- DrugBank_drug_groups[DrugBank_drug_groups$group == "approved", ]
+DrugBank_Drug_Target <- DrugBank_Drug_Target[DrugBank_Drug_Target$drug_id %in% DrugBank_drug_groups$`drugbank-id`, ]
+
+# Keep only drugs whose targets are in the network
+DrugBank_Drug_Target <- DrugBank_Drug_Target[DrugBank_Drug_Target$drug_target %in% c(drug_target_Net$from_geneSymbol, drug_target_Net$to_geneSymbol),]
+
+
+# Add the drug names
+DrugBank_Drugs <- read.csv("Data_prep/drug.csv", header = TRUE)
+DrugBank_Drug_Target$drug_name <- DrugBank_Drugs$name[match(DrugBank_Drug_Target$drug_id, DrugBank_Drugs$primary_key)]
+DrugBank_Drugs <- DrugBank_Drugs[DrugBank_Drugs$type == "small molecule", ]
+DrugBank_Drug_Target <- DrugBank_Drug_Target[DrugBank_Drug_Target$drug_id %in% DrugBank_Drugs$primary_key, ]
+
+
+DrugBank_Drug_Target <- DrugBank_Drug_Target[, c("drug_id", "drug_name", "drug_target", "name")]
+colnames(DrugBank_Drug_Target) <- c("drug_id", "drug_name", "drug_target", "drug_target_name")
+
+# Remove drugs that directly target ACE2
+DrugBank_Drug_Target <- DrugBank_Drug_Target[DrugBank_Drug_Target$drug_target != "ACE2", ]
+
+# Get drugs with only certain categories
+DrugBank_drug_categories <- read.csv("Data_prep/drug_categories.csv", header = TRUE)
+DrugBank_drug_categories <- DrugBank_drug_categories[DrugBank_drug_categories$category %in% c("Antiviral Agents", "Antiprotozoals", "Antiparasitic Agents"), ]
+DrugBank_Drug_Target <- DrugBank_Drug_Target[DrugBank_Drug_Target$drug_id %in% DrugBank_drug_categories$parent_key, ]
+
+
+# Filter to keep only the most distant targets from ACE2
+# If there are multiple targets with same distance, they are removed
+# Doing this to reduce the number of drugs for exercise
+network <- graph_from_data_frame(drug_target_Net, directed = FALSE)
+
+cc <- components(network)
+largest_components <- V(network)[cc$membership == which.max(cc$csize)] # remove connected components
+network <- induced_subgraph(network, largest_components)
+
+distance_table <- as.data.frame(distances(network, v = "ACE2", to = V(network)$name))
+
+
+final_df <- data.frame()
+for(drug in unique(DrugBank_Drug_Target$drug_id)){
+  tmp1 <- DrugBank_Drug_Target[DrugBank_Drug_Target$drug_id == drug, ]
+  
+  if(nrow(tmp1) == 1){
+    if(tmp1$drug_target %in% V(network)$name){
+      final_df <- rbind(final_df, tmp1)
+    }
+  }else{
+    tmp2 <- distance_table[names(distance_table) %in% tmp1$drug_target]
+    tmp1 <- tmp1[tmp1$drug_target %in% names(tmp2[which(tmp2 == max(tmp2))]),]
+    if(nrow(tmp1) == 1){
+      final_df <- rbind(final_df, tmp1)
+    }
+  }
+}
+
+
+
+write.csv(final_df, "Data/Drugs.csv", row.names = FALSE)
+
+# "Antiviral Agents", "Antiprotozoals", "Antiparasitic Agents", "Antivirals for Systemic Use",
+#"Experimental Unapproved Treatments for COVID-19", "Approved Treatments for COVID-19"
---
+++ Extras/Data_prep_E2.R
@@ -0,0 +1,43 @@
+library(igraph)
+library(tidyr)
+
+# Read network
+Barabasi_Net <- read.csv("Data/Barabasi_CoV2_PPI.csv")
+Barabasi_Net <- Barabasi_Net <- graph_from_data_frame(Barabasi_Net, directed = FALSE)
+
+
+# Read list of human genes interacting with SARS-Cov-2
+SarsCov_targets <- read.csv("Data/SARSCoV2_Targets.csv")
+
+# Create subnetwork with the covid targets
+Barabasi_SarsCov_Net <- induced_subgraph(Barabasi_Net, V(Barabasi_Net)[V(Barabasi_Net)$name %in% SarsCov_targets$EntrezID])
+Barabasi_SarsCov_Net <- simplify(Barabasi_SarsCov_Net, remove.loops = TRUE) # remove loops
+isolated <- names(which(degree(Barabasi_SarsCov_Net)==0)) # remove isolated nodes
+Barabasi_SarsCov_Net <- delete.vertices(Barabasi_SarsCov_Net, isolated)
+Barabasi_SarsCov_Net <- set_vertex_attr(Barabasi_SarsCov_Net, 
+                                        name = "clustering_coefficient", 
+                                        value = transitivity(Barabasi_SarsCov_Net, type = "local", isolates = "zero"))
+
+Barabasi_SarsCov_Net <- induced_subgraph(Barabasi_SarsCov_Net, V(Barabasi_SarsCov_Net)[V(Barabasi_SarsCov_Net)$clustering_coefficient > 0.5])
+selected_SarsCov_genes <- V(Barabasi_SarsCov_Net)$name
+
+
+# Read disease genes association
+disease_gene_links <- data.frame()
+for(line in readLines("Data/Guney2016_GenesDisease.tsv")){
+  tmp1 <- strsplit(line, "\t")
+  tmp2 <- data.frame(disease = tmp1[[1]][2],
+                     genes = paste(tmp1[[1]][3:length(tmp1[[1]])], collapse = ";"))
+  disease_gene_links <- rbind(disease_gene_links, tmp2)
+}
+disease_gene_links <- separate_rows(disease_gene_links, "genes", sep = ";")
+
+# Get disease genes from selected distant diseases from SARS-Cov-2
+distant_diseases = c("peroxisomal disorders", "cardiomyopathy, hypertrophic", "anemia", "sarcoma")
+dis_genes_select <- unique(disease_gene_links$genes[disease_gene_links$disease %in%  distant_diseases])
+
+# Create network for exercise
+Exercise_Net <- induced_subgraph(Barabasi_Net, 
+                                 V(Barabasi_Net)[V(Barabasi_Net)$name %in% c(selected_SarsCov_genes, dis_genes_select)])
+Exercise_Net <- as_data_frame(Exercise_Net, what = "edges")
+write.csv(Exercise_Net, "Exercise_PPI_Net.csv", row.names = FALSE)
